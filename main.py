c57bc7231561ed032256cf6158bb4c11eb9c45c3
papmatedaniel <papmd2014@gmail.com>
2024-02-24 22:55:04 +0100

    valtozonevekmodositasa

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///allatok.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Allat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    gender = db.Column(db.String(10), nullable=False)
+    nev = db.Column(db.String(80), nullable=False)
+    nem = db.Column(db.String(10), nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -17,45 +17,45 @@ with app.app_context():
 def index():
     return render_template('index.html')
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    name = request.form['name']
-    gender = request.form['gender']
-    new_user = User(name=name, gender=gender)
-    db.session.add(new_user)
+@app.route('/allat_hozzaadasa', methods=['POST'])
+def allat_hozzaadasa():
+    nev = request.form['nev']
+    nem = request.form['nem']
+    uj_allat = Allat(nev=nev, nem=nem)
+    db.session.add(uj_allat)
     db.session.commit()
     return redirect('/')
 
 
-@app.route('/felhasznalomodositasa')
-def felhasznalomodositasa():
-    users = User.query.all()
-    return render_template('felhasznalomodositasa.html', users=users)
+@app.route('/allatmodositasa')
+def allatmodositasa():
+    allatok = Allat.query.all()
+    return render_template('allatmodositasa.html', allatok=allatok)
 
-@app.route('/update_user', methods=['POST'])
-def update_user():
-    for user in User.query.all():
-        user_id = user.id
-        if f"update_{user_id}" in request.form['action']:
-            user.name = request.form[f'name{user_id}']
-            user.gender = request.form[f'gender{user_id}']
-        elif f"delete_{user_id}" in request.form['action']:
-            db.session.delete(user)
+@app.route('/allat_frissitese', methods=['POST'])
+def allat_frissitese():
+    for allat in Allat.query.all():
+        allat_id = allat.id
+        if f"update_{allat_id}" in request.form['action']:
+            allat.nev = request.form[f'nev{allat_id}']
+            allat.nem = request.form[f'nem{allat_id}']
+        elif f"delete_{allat_id}" in request.form['action']:
+            db.session.delete(allat)
     if "update_all" in request.form['action']:
-        for user in User.query.all():
-            user.name = request.form[f'name{user.id}']
-            user.gender = request.form[f'gender{user.id}']
+        for allat in Allat.query.all():
+            allat.nev = request.form[f'nev{allat.id}']
+            allat.nem = request.form[f'nem{allat.id}']
     db.session.commit()
-    return redirect('/table')
+    return redirect('/tablazat')
 
 
 
 
 
-@app.route('/table')
-def table():
-    users = User.query.all()
-    return render_template('table.html', users=users)
+@app.route('/tablazat')
+def tablazat():
+    users = Allat.query.all()
+    return render_template('tablazat.html', users=users)
 
 
 if __name__ == '__main__':
